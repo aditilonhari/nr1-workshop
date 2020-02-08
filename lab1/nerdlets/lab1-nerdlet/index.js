225fc99e7bf570b24e4462eb8c39088f8d826bae
Aditi <alonhari@newrelic.com>
2020-02-08 10:45:34 -0800

    Working with Chart components

--- lab1/nerdlets/lab1-nerdlet/index.js
+++ lab1/nerdlets/lab1-nerdlet/index.js
@@ -1,37 +1,102 @@
 import React from "react";
-import { TableChart, Stack, StackItem } from "nr1";
+import {
+  TableChart,
+  Stack,
+  StackItem,
+  ChartGroup,
+  LineChart,
+  ScatterChart
+} from "nr1";
 
 // https://docs.newrelic.com/docs/new-relic-programmable-platform-introduction
 
 export default class Lab1NerdletNerdlet extends React.Component {
   constructor(props) {
     super(props);
-    this.accountId = 2586072;
+    this.accountId = 2596214;
     this.state = {
       appId: null,
       appName: null
     };
-    console.debug("Nerdlet constructor", this); //eslint-disable-line
+    console.debug("Nerdlet constructor", this);
+  }
+
+  setApplication(inAppId, inAppName) {
+    this.setState({ appId: inAppId, appName: inAppName });
   }
 
   render() {
     const { appId, appName } = this.state;
     const nrql = `SELECT count(*) as 'transactions', apdex(duration) as 'apdex', percentile(duration, 99, 90, 70) FROM Transaction facet appName, appId limit 25`;
+    const tCountNrql = `SELECT count(*) FROM Transaction WHERE appId = ${appId} TIMESERIES`;
+    const apdexNrql = `SELECT apdex(duration) FROM Transaction WHERE appId = ${appId} TIMESERIES`;
     //return the JSX we're rendering
     return (
-      <Stack
-        verticalType={Stack.VERTICAL_TYPE.FILL}
-        directionType={Stack.DIRECTION_TYPE.VERTICAL}
-        gapType={Stack.GAP_TYPE.EXTRA_LOOSE}
-      >
-        <StackItem>
-          <TableChart
-            query={nrql}
-            accountId={this.accountId}
-            className="chart"
-          />
-        </StackItem>
-      </Stack>
+      <ChartGroup>
+        <Stack
+          verticalType={Stack.VERTICAL_TYPE.FILL}
+          directionType={Stack.DIRECTION_TYPE.VERTICAL}
+          gapType={Stack.GAP_TYPE.EXTRA_LOOSE}
+        >
+          <StackItem>
+            <Stack
+              horizontalType={Stack.HORIZONTAL_TYPE.FILL}
+              directionType={Stack.DIRECTION_TYPE.HORIZONTAL}
+              gapType={Stack.GAP_TYPE.EXTRA_LOOSE}
+            >
+              <StackItem>
+                <TableChart
+                  query={nrql}
+                  accountId={this.accountId}
+                  className="chart"
+                  onClickTable={(dataEl, row, chart) => {
+                    //for learning purposes, we'll write to the console.
+                    console.debug([dataEl, row, chart]); //eslint-disable-line
+                    this.setApplication(row.appId, row.appName);
+                  }}
+                />
+              </StackItem>
+              <StackItem>
+                <LineChart
+                  query={`SELECT count(*) as 'transactions' FROM Transaction facet appName, appId limit 25 TIMESERIES`}
+                  className="chart"
+                  accountId={this.accountId}
+                  onClickLine={line => {
+                    //more console logging for learning purposes
+                    console.debug(line); //eslint-disable-line
+                    const params = line.metadata.label.split(",");
+                    this.setApplication(params[1], params[0]);
+                  }}
+                />
+              </StackItem>
+            </Stack>
+          </StackItem>
+          {appId && (
+            <StackItem>
+              <Stack
+                horizontalType={Stack.HORIZONTAL_TYPE.FILL}
+                directionType={Stack.DIRECTION_TYPE.HORIZONTAL}
+                gapType={Stack.GAP_TYPE.EXTRA_LOOSE}
+              >
+                <StackItem>
+                  <LineChart
+                    accountId={this.accountId}
+                    query={tCountNrql}
+                    className="chart"
+                  />
+                </StackItem>
+                <StackItem>
+                  <ScatterChart
+                    accountId={this.accountId}
+                    query={apdexNrql}
+                    className="chart"
+                  />
+                </StackItem>
+              </Stack>
+            </StackItem>
+          )}
+        </Stack>
+      </ChartGroup>
     );
   }
 }
